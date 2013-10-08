0261705af2243c29a2e4c53c634dab50350e0f55
Jonathan Jackson <jonathan.jackson1@me.com>
2013-10-08 08:44:58 -0400

    Add q promises library

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var exec = require('child_process').exec,
+var Q = require('q'),
+    exec = require('child_process').exec,
     util = require('util'),
-    eak_url = "https://github.com/stefanpenner/ember-app-kit.git";
 
 function EmberAppKitNew(appName) {
-  this.cloneRepo(appName)
+  Q.fcall(this.cloneRepo(appName))
+    .then(this.exit)
+    .done();
 }
 
 EmberAppKitNew.prototype.cloneRepo = function(appName) {
-  command = "git clone " + eak_url + " " + appName;
-  util.puts("Running: `" + command + "`");
+  var eak_url = "https://github.com/stefanpenner/ember-app-kit.git",
+      command = "git clone " + eak_url + " " + appName;
 
-  exec(appName, function(error,stdout,stderr) {
+  exec(command);
+}
 
-  });
+EmberAppKitNew.prototype.exit = function(){
+  util.puts("Finished!");
 }
 
 module.exports = EmberAppKitNew;
