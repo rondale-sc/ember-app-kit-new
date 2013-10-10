427926a701cb955e963d14b322331d52841ff4f6
Brandon Farmer and Jonathan Jackson <dev+bthesorceror+rondale-sc@hashrocket.com>
2013-10-09 22:35:25 -0400

    Add cloneRepo, cleanRepo, installGruntCli to EAKnew

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-var Q = require('q'),
-    exec = require('child_process').exec,
+var exec = require('child_process').exec,
+    path = require('path'),
+    fs   = require('fs-extra'),
     util = require('util'),
+    readline = require('readline');
 
 function EmberAppKitNew(appName) {
-  Q.fcall(this.cloneRepo(appName))
-    .then(this.exit)
-    .done();
+  this.appName = appName;
 }
 
-EmberAppKitNew.prototype.cloneRepo = function(appName) {
+util.inherits(EmberAppKitNew, (require('events')).EventEmitter);
+
+EmberAppKitNew.prototype.cloneRepo = function() {
   var eak_url = "https://github.com/stefanpenner/ember-app-kit.git",
-      command = "git clone " + eak_url + " " + appName;
+      command = "git clone " + eak_url + " " + this.appName;
 
-  exec(command);
+  var child = exec(command);
+
+  child.on('close', function() {
+    this.emit('repoCloned');
+  }.bind(this));
 }
 
-EmberAppKitNew.prototype.exit = function(){
-  util.puts("Finished!");
+EmberAppKitNew.prototype.cleanRepo = function(){
+  fs.remove(path.join(this.appName, '.git'), function(err) {
+    if (err) { console.log(err); this.emit('error', err); return; }
+
+    this.emit('repoCleaned');
+  }.bind(this));
 }
+
+EmberAppKitNew.prototype.installGruntCli = function(){
+  process.stdin.once('data', function(answer) {
+    if(answer.trim()==="y") {
+      var command = 'grunt'
+      this.emit(command + " installing");
+      this.runNpmCommand();
+    }
+  }.bind(this));
+
+  process.stdout.write("Do you want to install grunt-cli globally? (y/n)\n");
+}
+
+EmberAppKitNew.prototype.runNpmCommand = function(commandString, global){
+  var command = "npm install " + (global ? "-g " : "") + commandString
+  child = exec(command);
+
+  child.on('close', function(){
+    this.emit(commandString + " installed");
+  });
+};
+
 
 module.exports = EmberAppKitNew;
--- spec/ember_app_kit_new_spec.js
+++ spec/ember_app_kit_new_spec.js
@@ -1,18 +1,90 @@
-var E = require('../index'),
-    helpers = require('./spec_helper'),
-    fs = require('fs');
+var EmberAppKitNew = require('../index');
+var tape           = require('tape');
+var fs             = require('fs-extra');
+var path           = require('path');
+var exec           = require('child_process').exec;
+var readline       = require('readline');
+
+function hook_stdout(callback) {
+  var old_write = process.stdout.write
+
+  process.stdout.write = (function(write) {
+    return function(string, encoding, fd) {
+      write.apply(process.stdout, arguments)
+      callback(string, encoding, fd)
+    }
+  })(process.stdout.write)
+
+  return function() {
+    process.stdout.write = old_write
+  }
+}
 
 (function(){
-  describe("creation", function() {
-    var appName = "bad_ass_app";
+  var appName = "/tmp/test_ember_appkit";
+  var cleanup = function() {
+    fs.remove(appName, function(err) {
+      if (err) { console.log(err); }
+    });
+  }
 
-    it("creates the directory", function(){
-      new E(appName);
-      expect(helpers.checkPath(appName)).toBeTruthy();
+  tape('cloneRepo', function(t){
+    t.plan(1);
+
+    var kit = new EmberAppKitNew(appName);
+
+    kit.cloneRepo();
+
+    kit.on('repoCloned', function() {
+      fs.exists(appName, function(exists) {
+        t.ok(exists, "EAKit directory exists");
+      })
     });
 
-    afterEach(function() {
-      fs.rmdir(appName);
-    });
+    t.on('end', cleanup);
   });
+
+  tape('cleanRepo', function(t) {
+    t.plan(2);
+    var kit = new EmberAppKitNew(appName);
+
+    kit.on('repoCloned', function() {
+      fs.exists(path.join(appName, '.git'), function(exists) {
+        t.ok(exists, '.git directory exists.');
+        kit.cleanRepo();
+      });
+    });
+
+    kit.on('repoCleaned', function() {
+      fs.exists(path.join(appName, '.git'), function(exists) {
+        t.notOk(exists, '.git sub-directory was removed!');
+      });
+    });
+
+    kit.cloneRepo();
+
+    t.on('end', cleanup);
+  });
+
+  tape('installGruntCli', function(t){
+    t.plan(3);
+
+    var kit = new EmberAppKitNew(appName);
+    kit.runNpmCommand = function(){
+      t.ok(true, "npm command ran");
+    };
+
+    var unhook = hook_stdout(function(data) {
+      unhook();
+      t.equal(data, "Do you want to install grunt-cli globally? (y/n)\n");
+      process.stdin.emit('data', "y\n");
+    });
+
+    kit.on('grunt installing', function(){
+      t.ok(true);
+    });
+
+    kit.installGruntCli();
+  });
+
 }).call(this);
